d1afa1f4522b72025452ce1e797ee0f25ebc93b5
Vlad Laptop <vladg13012007@gmail.com>
2025-10-10 20:25:09 +0300

    migrations

---
+++ src/Services/AutoCatalog/AutoCatalog.Infrastructure/Migrations/20251010120744_DecimalCarProperties.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoCatalog.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DecimalCarProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Consumption",
+                table: "Cars",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Acceleration",
+                table: "Cars",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Consumption",
+                table: "Cars",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Acceleration",
+                table: "Cars",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+    }
+}
